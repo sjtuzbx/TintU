8bf78744b8678fd1899f8cf50365165da9271e7a
Zhangbenxuan <sjtuzhangbenxuan@163.com>
2018-05-05 10:32:37 +0800

    First template

---
+++ pages/services/services.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    content: '微信绑定内容',
+    hiddencontent: '隐藏的内容',
+    flag: false,
+    num1: 1,
+    num2: 2,
+    user: {
+      name: "spark",
+      age: 18
+    }
+  }
+})
